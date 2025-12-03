e233136da6a589264827ab6e7a5850d6a4c169dd
denn-p <dennielysp@gmail.com>
2025-12-03 13:28:01 +0000

    card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-import "bootstrap";
-import "./style.css";
+const numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "J", "K", "A"];
+
+const icons = ["♦", "♥", "♠", "♣"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function changeCard() {
+  let i = Math.floor(Math.random() * numbers.length);
+
+  let y = Math.floor(Math.random() * icons.length);
+  
+  let valueElement = document.querySelector("#value")
+  valueElement.textContent = numbers[i]
+
+
+  let topIconElement = document.querySelector(".top-icon")
+  topIconElement.textContent = icons[y]
+
+  let bottonIconElement = document.querySelector(".bottom-icon")
+  bottonIconElement.textContent = icons[y]
+
+  let card = document.querySelector(".card")
+  if (icons[y] === "♥" || icons[y] === "♦") {
+    card.style.color = "red";
+  }
+  else {
+    card.style.color = "black";
+  }
+
+}
+window.onload = function () {
+  changeCard()
+}
+
+let button = document.getElementById("button");
+button.addEventListener("click", changeCard)
+
+
+
+console.log(icons[y])
